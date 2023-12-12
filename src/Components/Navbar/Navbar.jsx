70fb026779f3614e3a7a171d4f643c365dcf10bd
Stephen Craig <Robertduck096@gmail.com>
2023-12-12 17:09:10 -0600

    First commit for Stephen Craig's Website

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+      <div className="n-wrapper">
+        <div className="n-left">
+          <div className="n-name">Stephen Craig</div>
+          <span>toggle</span>
+        </div>
+        <div className="n-right">
+          <div className="n-list">
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+              <li>Education</li>
+              <li>Portfolio</li>
+            </ul>
+          </div>
+          <button className="button">Contact Me</button>
+        </div>
+      </div>
+    )
+  }
+
+  export default Navbar
